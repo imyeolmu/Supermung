be09cda54edfbedf7457f8070899ba43a94041c9
yh <wjddbsgml0126@gmail.com>
2022-07-27 13:00:44 +0900

    xml 설정 변경..

--- src/main/webapp/WEB-INF/views/admin/member/member-point.jsp
+++ src/main/webapp/WEB-INF/views/admin/member/member-point.jsp
@@ -1,4 +1,4 @@
-<%@page import="com.supermm.model.MemberVO"%>
+
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
--- src/test/java/com/supermm/persistence/DataSourceTest.java
+++ src/test/java/com/supermm/persistence/DataSourceTest.java
@@ -11,7 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class DataSourceTest {
